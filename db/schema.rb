cd77a890811e4c6da213bbf87686016e0a771fc2
kanegongoon <101990076+kanegongoon@users.noreply.github.com>
2022-05-12 15:03:56 +0900

    ペット情報登録

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_08_072606) do
+ActiveRecord::Schema.define(version: 2022_05_09_060259) do
+
+  create_table "information", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "sex", null: false
+    t.string "kinds"
+    t.string "color", null: false
+    t.date "birthday", null: false
+    t.string "pet_image_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_information_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +40,5 @@ ActiveRecord::Schema.define(version: 2022_05_08_072606) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "information", "users"
 end
